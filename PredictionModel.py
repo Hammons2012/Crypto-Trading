bb30526390278da88e1f1a253c27ca151c2731ce
Hammon2012 <51793666+Hammons2012@users.noreply.github.com>
2022-10-18 12:23:11 +0000

    Update PredictionModel.py

--- PredictionModel.py
+++ PredictionModel.py
@@ -10,8 +10,15 @@ import tensorflow
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 from Credentials import credentials
+
+# Creating variables that will be used trhoughout the script.
+test_size = 30 # in days
+length = 25
+n_features = 2
 
 # Creating connection to PG for psycopg2.
 # Note that this script assumes that the database has already been created.
@@ -30,11 +37,11 @@ cur = conn.cursor()
 
 #Columns of price data to use
 cur.execute("SELECT date, close FROM daily_sma;")
-data = pandas.DataFrame(cur.fetchall(), columns=['date', 'close'])
+data = pandas.DataFrame(cur.fetchall(), columns = ['date', 'close'])
 print(data)
 
 # Plotting the SQL database to a graph.
-plt.plot(data["date"], data["close"], label="BTC Price")
+plt.plot(data['date'], data['close'], label = "BTC Price")
 plt.title("BTC-USD Price")
 plt.xlabel("Date")
 plt.ylabel("Price in USD")
@@ -42,28 +49,50 @@ plt.legend()
 plt.show()
 
 # Formatting data to prepare it for prediction.
-X = data["close"].values.reshape(-1,1)
-#y = data["date"].values.astype(pandas.Float32Dtype)
-y = data["date"].values.astype(pandas.DatetimeIndex)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
-X_train.shape
-X_test.shape
+data['date'] = pandas.to_numeric(pandas.to_datetime(data['date']))
+test_indicators = len(data) - test_size
+train = data.iloc[:test_indicators]
+test = data.iloc[test_indicators:]
 scaler = MinMaxScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_train)
-
-# Testing pulling index values.
-print(data['date'].astype(str))
+scaler.fit(train)
+scaled_train = scaler.transform(train)
+scaled_test = scaler.transform(test)
+generator = TimeseriesGenerator(scaled_train, scaled_train, length = length, batch_size = 1)
+X, y = generator[0]
 
 # Creating the model for prediction.
+early_stop = EarlyStopping(monitor = 'val_loss', patience = 2)
+validation_generator = TimeseriesGenerator(scaled_test, scaled_test, length = length, batch_size = 1)
+
 model = Sequential()
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='relu'))
+model.add(LSTM(100, activation = 'relu', input_shape = (length, n_features)))
 model.add(Dense(1))
 model.compile(
-    optimizer = 'rmsprop', 
+    optimizer = 'adam',
     loss = 'mse'
 )
-model.fit(x = X_train, y = y_train, epochs = 250)
+model.summary()
+model.fit(
+    generator, 
+    epochs = 20, 
+    validation_data = validation_generator, 
+    callbacks = early_stop
+)
+
+# Plotting losses.
+losses = pandas.DataFrame(model.history.history)
+losses.plot()
+plt.show()
+
+test_predictions = []
+first_eval_batch = scaled_train[-length:]
+current_batch = first_eval_batch.reshape((1, length, n_features))
+
+for i in range(len(test)):
+    current_pred = model.predict(current_batch)[0]
+    test_predictions.append(current_pred)
+    current_batch = numpy.append(current_batch[:,2:,:], [[current_pred]], axis = 1)
+
+true_predictions = scaler.inverse_transform(test_predictions)
+test['Predictions'] = true_predictions
+test.plot(figsize = (12, 8))
